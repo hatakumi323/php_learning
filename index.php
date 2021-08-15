fbc6f43bffe1a731b3e276491552ab3629badda7
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-08-15 17:39:38 +0900

    function mail

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <?php 
    
-$members_2 = [
-    '本田' => [
-        'name' => '本田',
-        'height' => 170,
-        'hobby' => 'サッカー'
-    ],
-    '香川' => [
-        'name' => '香川',
-        'height' => 160,
-        'hobby' => 'サッカー'
-    ]
-];
+$postalCode = '123-4567';
 
-foreach($members_2 as $members_1){
-    foreach($members_1 as $member => $value){
-        echo $member . 'は' . $value . 'です。';
-        echo "<br/>";
+function checkPostalCode($str){
+    $replaced = str_replace('-','',$str);
+    $length = strlen($replaced);
+
+    var_dump($length);
+
+    if($length === 7){
+        return true;
     }
+    return false;
 }
+
+var_dump(checkPostalCode($postalCode));
 
 ?>
