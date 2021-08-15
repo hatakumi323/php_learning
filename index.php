3d9a4d214e8bd7ae944043019f2c6579b7f6def4
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-08-15 14:26:07 +0900

    constant

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+   
+$array_1 = [1,2,3];
+
+$array_2 = [
+    ['赤','青','黄'],
+    ['緑','紫','黒']
+];
+
+echo $array_2[1][1];
+
+$array_member = [
+    'name' => '本田',
+    'height' => 170,
+    'hobby' => 'サッカー'
+];
+
+echo $array_member['hobby'];
+
+$array_member_2 = [
+    '本田' => [
+        'height' => 170,
+        'hobby' => 'サッカー'
+    ],
+    '香川' => [
+        'height' => 165,
+        'hobby' => 'サッカー'
+    ]
+    ];
+
+    echo $array_member_2['香川']['height'];
+
+
+    $array_member_3 = [
+        '1kumi' => [
+             '本田' => [
+                 'height' => 170,
+                 'hobby' => 'サッカー' 
+             ],
+             '香川' => [
+                 'height' => 165,
+                 'hobby' => 'サッカー'
+             ]
+        ],
+        '2kumi' => [
+            '長友' => [
+                'height' => 160,
+                'hobby' => 'サッカー'
+            ],
+            '乾' => [
+                'height' => 168,
+                'hobby' => 'サッカー'
+            ]
+        ]
+        ];
+
+        echo $array_member_3['2kumi']['長友']['height'];
+
+        echo '<pre>';
+        var_dump($array_member_3);
+        echo '</pre>';
+?>
