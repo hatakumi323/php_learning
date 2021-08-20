e839160f7e764446e86ff6a4c3fa67ec90ed5030
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-08-20 23:05:21 +0900

    basic auth

--- form/input.php
+++ form/input.php
@@ -38,16 +38,16 @@ if(!empty($_POST['btn_submit'])){
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="ja">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <title>私は仕事が大好きです。</title>
+    <title>Hello, world!</title>
   </head>
 <body>
 
@@ -129,58 +129,77 @@ $token = $_SESSION['csrfToken'];
 
 <?php endif ;?>
 
-<form method="POST" action="input.php">
-氏名
-<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;} ?>">
-<br>
-メールアドレス
-<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;} ?>">
-<br>
-ホームページ
-<input type="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']) ;} ?>">
-<br>
-性別
-<input type="radio" name="gender" value="0" 
-<?php if(isset($_POST['gender']) && $_POST['gender'] === '0' )
-{ echo 'checked'; } ?>>男性
-<input type="radio" name="gender" value="1"
-<?php if(isset($_POST['gender']) && $_POST['gender'] === '1' )
-{ echo 'checked'; } ?>>女性
-<br>
-年齢
-<select name="age">
-  <option value="">選択してください</option>
-  <option value="1">〜19歳</option>
-  <option value="2">20歳〜29歳</option>
-  <option value="3">30歳〜39歳</option>
-  <option value="4">40歳〜49歳</option>
-  <option value="5">50歳〜59歳</option>
-  <option value="6">60歳〜</option>
-</select> 
-<br>
-お問い合わせ内容
-<textarea name="contact">
-<?php if(!empty($_POST['contact'])){echo h($_POST['contact']) ;} ?>
-</textarea>
-<br>
-<input type="checkbox" name="caution" value="1">注意事項にチェックする
-<br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+    <form method="POST" action="input.php">
+    <div class="form-group">
+      <label for="your_name">氏名</label>
+      <input type="text" class="form-control" id="your_name" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;} ?>" required>
+    </div>
 
-<input type="submit" name="btn_confirm" value="確認する">
-<input type="hidden" name="csrf" value="<?php echo $token; ?>">
-</form>
+    <div class="form-group">
+      <label for="email">メールアドレス</label>
+      <input type="email" class="form-control" id="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;} ?>" required>
+    </div>
+
+    <div class="form-group">
+      <label for="url">ホームページ</label>
+      <input type="url" class="form-control" id="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']) ;} ?>">
+    </div>
+
+性別
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="gender" id="gender1" value="0" 
+      <?php if(isset($_POST['gender']) && $_POST['gender'] === '0' )
+      { echo 'checked'; } ?>>
+      <label class="form-check-label">男性</label>
+      <input class="form-check-input" type="radio" name="gender" id="gender2" value="1"
+      <?php if(isset($_POST['gender']) && $_POST['gender'] === '1' )
+      { echo 'checked'; } ?>>
+      <label class="form-check-label">女性</label>
+    </div>
+
+    <div class="form-group">
+    <label for="age">年齢</label>
+    <select class="form-control" id="age" name="age">
+      <option value="">選択してください</option>
+      <option value="1">〜19歳</option>
+      <option value="2">20歳〜29歳</option>
+      <option value="3">30歳〜39歳</option>
+      <option value="4">40歳〜49歳</option>
+      <option value="5">50歳〜59歳</option>
+      <option value="6">60歳〜</option>
+    </select> 
+    </div>
+
+    <div class="form-group">
+      <label for="contact">お問い合わせ内容</label>
+      <textarea class="form-control" id="contact" row="3" name="contact">
+      <?php if(!empty($_POST['contact'])){echo h($_POST['contact']) ;} ?>
+      </textarea>
+    </div>
+
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="caution" name="caution" value="1">
+      <label class="form-check-label" for="caution">注意事項にチェックする</label>
+    </div>
+    
+    <input class="btn btn-info" type="submit" name="btn_confirm" value="確認する">
+    <input type="hidden" name="csrf" value="<?php echo $token; ?>">
+    </form>
+
+    </div><!-- .col-md-6 -->
+  </div>
+</div>
+
 <?php endif; ?>
 
-<!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    -->
+ <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </body>
 </html>
 
---
+++ mainte/test.php
@@ -0,0 +1,8 @@
+<?php 
+    echo __FILE__;
+    // /Applications/MAMP/htdocs/php_learning/mainte/test.php
+    echo '<br>';
+    // パスワード暗号化
+    echo (password_hash('password123', PASSWORD_BCRYPT))
+    
+?>
