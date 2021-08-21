5da0d00468dd41f36b746a8636826c0d304b295d
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-08-21 17:30:49 +0900

    db_connection

--- mainte/test.php
+++ mainte/test.php
@@ -3,6 +3,17 @@
     // /Applications/MAMP/htdocs/php_learning/mainte/test.php
     echo '<br>';
     // パスワード暗号化
-    echo (password_hash('password123', PASSWORD_BCRYPT))
+    echo (password_hash('password123', PASSWORD_BCRYPT));
     
+
+    // $contactFile = '.contact.dat';
+
+
+    // $fileContents = file_get_contents($contactFile);
+
+    // echo $fileContents;
+
+    // file_put_contents($contactFile, 'テストです。');
+
+     
 ?>
