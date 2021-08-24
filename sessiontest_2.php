18350fe04ef724813b589d12cb15449729e480ab
takumi <65800363+hatakumi323@users.noreply.github.com>
2021-08-24 22:31:18 +0900

    session2

---
+++ sessiontest_2.php
@@ -0,0 +1,45 @@
+<?php
+
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <?php
+
+  echo 'セッションを破棄しました。';
+
+  $_SESSION = [];
+
+  if (isset($_COOKIE['PHPSESSID'])) {
+    setcookie('PHPSESSID', '', time() - 1800, '/');
+  }
+
+  session_destroy();
+
+  echo 'セッション';
+  echo '<pre>';
+  echo var_dump($_SESSION);
+  echo '</pre>';
+
+  echo 'クッキー';
+  echo '<pre>';
+  echo var_dump($_COOKIE);
+  echo '</pre>';
+
+  ?>
+</body>
+
+</html>
